8244e52fc93b2faa24ed66eaa3077d81517a24e5
KrishnaM023 <krishna050120>
2024-04-06 14:54:47 +0530

    File with 302 response

--- server.js
+++ server.js
@@ -1,30 +1,47 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
 
-     if(url === '/home') {
-        //res.setHeader('Content-Type', 'text/html');
+    if(url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type"text" name="message"><button>Enter</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+
+    // Wrting to the file and redirect it with 302
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    else if(url === '/home') {
         res.write('<html>');
         res.write('<head><title>Home</title></head>');
         res.write('<body><h1>Welcome to Home</h1></body>');
         res.write('</html>');
         return res.end();
     } else if(url === '/home') {
-        //res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>About</title></head>');
         res.write('<body><h1>Welcome to About Us Page</h1></body>');
         res.write('</html>');
         return res.end();
     } else if(url === '/node') {
-        //res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Node</title></head>');
         res.write('<body><h1>Welcome to my Node JS Project</h1></body>');
         res.write('</html>');
         return res.end();
     }
+
     else{
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
