712f0c01ab638a7c0f5b4598da1e05fb915326d9
KrishnaM023 <krishna050120>
2024-05-01 10:42:48 +0530

    Express starts

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const http = require('http');
+//const routes = require('./routes');
+//console.log(routes.someText);
 
-const routes = require('./routes');
-console.log(routes.someText);
+const express = require('express');
+const app = express();
 
-const server = http.createServer(routes.handler);
-
-server.listen(4000, () => {
-    console.log("Server is running on port number 4000");
+app.use((req, res, next) => {
+    console.log('In the Middleware');
+    next();  // Allows the request to continue to the next middleware
 });
+
+app.use((req, res, next) => {
+    console.log('In another Middleware');
+    res.send('<h1>Hello from Express!</h1>')
+});
+
+app.listen(3000);
